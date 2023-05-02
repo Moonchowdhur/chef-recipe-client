c98fac4d32c784ab0828db4581613f0c1ab91ebb
kaushani <kaushanichowdhury52@gmail.com>
2023-05-02 09:30:04 +0530

    changed

--- src/pages/Eachchef/Eachchef.jsx
+++ src/pages/Eachchef/Eachchef.jsx
@@ -7,6 +7,8 @@ import "react-toastify/dist/ReactToastify.css";
 const Eachchef = () => {
   const chefDetails = useLoaderData();
   const [love, setLove] = useState(true);
+  const [con, setCon] = useState(true);
+  const [lo, setLo] = useState(true);
   console.log(chefDetails);
   const {
     bio,
@@ -29,10 +31,17 @@ const Eachchef = () => {
   };
 
   const changeLove2 = () => {
-    if (love === true) {
+    if (con === true) {
       toast("Recipe is your favorite");
     }
-    setLove(false);
+    setCon(false);
+  };
+
+  const changeLove3 = () => {
+    if (lo === true) {
+      toast("Recipe is your favorite");
+    }
+    setLo(false);
   };
   return (
     <div className=" p-4 md:px-12">
@@ -119,7 +128,7 @@ const Eachchef = () => {
               <button onClick={() => changeLove2()}>
                 <BsFillSuitHeartFill
                   className={` text-3xl ${
-                    love ? "text-red-500" : "text-gray-500"
+                    con ? "text-red-500" : "text-gray-500"
                   }`}
                 />
               </button>
@@ -130,13 +139,49 @@ const Eachchef = () => {
             <p className="mt-4">
               <span className="font-bold">Method:</span> {recipe2?.method}
             </p>
-            <p className="mt-4 font-bold text-[#41644A]">
+            <p className="mt-4  font-bold text-[#41644A]">
               Rating: {recipe2?.rating}
             </p>
           </div>
         </div>
       </div>
       {/* 2nc recipe end */}
+      {/* 3rd recipe start */}
+      <div className="mt-7 ">
+        <div className="md:flex gap-6 shadow-lg p-4 shadow-green-800">
+          <div className=" md:w-[30%]  ">
+            <img
+              src={recipe3?.pic}
+              className="w-[400px] rounded-lg h-[350px]"
+              alt=""
+            />
+          </div>
+          <div className="mt-5 md:mt-0 md:w-[50%]">
+            <div className="flex  justify-between items-center">
+              <h2 className="text-3xl text-[#41644A] font-bold">
+                Recipe name:{recipe3?.recipe_name}
+              </h2>
+              <button onClick={() => changeLove3()}>
+                <BsFillSuitHeartFill
+                  className={` text-3xl ${
+                    lo ? "text-red-500" : "text-gray-500"
+                  }`}
+                />
+              </button>
+            </div>
+            <p className="mt-4 text-xl font-medium">
+              Ingredients: {recipe3?.ingredients}
+            </p>
+            <p className="mt-4">
+              <span className="font-bold">Method:</span> {recipe3?.method}
+            </p>
+            <p className="mt-4  font-bold text-[#41644A]">
+              Rating: {recipe3?.rating}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 3rd recipe end */}
       <ToastContainer />
     </div>
   );
